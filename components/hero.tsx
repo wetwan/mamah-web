9d8ad23cf37fb82072cc2fe72107a1f796a66e24
ridwanullah <wetwan080@gmail.com>
2025-10-06 19:37:39 +0100

    image added for herro

---
+++ components/hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  const banner = [
+    {
+      image:
+        "https://www.pngfind.com/pngs/m/113-1137886_gold-shoes-image-gold-glitter-heels-png-transparent.png",
+      text: "",
+      link: "/shop",
+    },
+    {
+      image:
+        "https://www.pngfind.com/pngs/m/113-1137886_gold-shoes-image-gold-glitter-heels-png-transparent.png",
+      text: "",
+      link: "/shop",
+    },
+  ];
+
+  return <div>Hero</div>;
+};
+
+export default Hero;
--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="text-neutral-700 flex items-center justify-center border-neutral-500 uppercase text-[20px] my-2 p-4 py-2  border-2 rounded  ">
+    <div className="text-neutral-700 flex items-center justify-center border-neutral-500 uppercase text-[20px] my-2 p-4 py-2  border-2 rounded cursor-pointer tracking-wide">
       uam closets
     </div>
   );
