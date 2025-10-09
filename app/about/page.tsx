6154c0121df64f27a5c01fd7f29fb0027a4c43e1
ridwanullah <wetwan080@gmail.com>
2025-10-09 07:55:03 +0100

    about page, and fontcahnges to poppins and roboto

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,32 @@ import React from "react";
 import image from "@/public/mamah.png";
 import Image from "next/image";
 import H2 from "@/components/h2";
+import { CircleQuestionMark, RotateCw, TruckIcon } from "lucide-react";
 
 const About = () => {
+  const policy = [
+    {
+      id: 1,
+      title: "Free Shipping",
+      context:
+        "Free Shipping Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.",
+      icon: <TruckIcon size={40} />,
+    },
+    {
+      id: 3,
+      title: "Free Returns",
+      context:
+        "Free Shipping Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.",
+      icon: <RotateCw size={40} />,
+    },
+    {
+      id: 2,
+      title: "Customer Support",
+      context:
+        "Free Shipping Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.",
+      icon: <CircleQuestionMark size={40} />,
+    },
+  ];
   return (
     <div>
       <div className="w-full  py-6 bg-[#f8f9fa] md:px-8 lg:px-16 xl:32 2xl:px-64   capitalize flex items-center gap-2 px-4 font-medium">
@@ -13,13 +37,39 @@ const About = () => {
         </Link>
         / <p>sucees</p>
       </div>
-      <div className="md:px-8 lg:px-16 xl:32 2xl:px-64 px-4 mt-20 flex flex-col lg:flex-row lg:h-[calc(100vh-500px)] border gap-10 py-5">
-        <div className="w-full lg:w-1/2  relative">
+      <div className="md:px-8 lg:px-16 xl:32 2xl:px-64 px-4 mt-20 flex flex-col lg:flex-row lg:h-[calc(100vh-300px)] border gap-10 py-5">
+        <div className="w-full md:w-1/2 mx-auto  max-lg:h-[400px] lg:w-1/2  relative">
           <Image src={image} alt="about" sizes="" fill className="" />
         </div>
-        <div className="w-full lg:w-1/2 border">
-          <H2 text="How We Started" className="flex items-start" />
+        <div className="w-full lg:w-1/2 ">
+          <H2 text="How We Started" className="flex items-start m-3" />
+
+          <div className="p-4 px-7 flex flex-col gap-4 font-light text-gray-600">
+            <p className="">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              repellat, dicta at laboriosam, nemo exercitationem itaque eveniet
+              architecto cumque, deleniti commodi molestias repellendus quos
+              sequi hic fugiat asperiores illum. Atque, in, fuga excepturi
+              corrupti error corporis aliquam unde nostrum quas.
+            </p>
+            <p className="">
+              Accusantium dolor ratione maiores est deleniti nihil? Dignissimos
+              est, sunt nulla illum autem in, quibusdam cumque recusandae,
+              laudantium minima repellendus.
+            </p>
+          </div>
         </div>
+      </div>
+      <div className="md:px-8 lg:px-16 xl:32 2xl:px-64 px-4 mt-20 flex flex-col lg:flex-row  border gap-6 items-center justify-between py-5">
+        {policy.map((policy) => (
+          <div key={policy.id} className="flex gap-5 flex-row">
+            <p className="text-[#7971ea]">{policy.icon}</p>
+            <div className="">
+              <h4 className="font-bold text-2xl mb-4">{policy.title}</h4>
+              <h4>{policy.context}</h4>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Poppins({
   subsets: ["latin"],
+  weight: "100",
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistMono.className} antialiased`}
       >
         <Nav />
         {children}
