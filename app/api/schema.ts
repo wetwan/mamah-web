6744725a5058018da40afcf62614dc6b7c6cfdf6
ridwanullah <wetwan080@gmail.com>
2025-10-09 22:12:01 +0100

    shippingSchema

--- app/api/schema.ts
+++ app/api/schema.ts
@@ -14,6 +14,18 @@ export const signUpSchema = z.object({
     address: z.string().min(3, 'First name most have atleast 3 characters'),
     phone: z.string().min(3, 'First name most have atleast 3 characters'),
 });
+export const shippingSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    firstName: z.string().min(3, 'First name most have atleast 3 characters'),
+    lastName: z.string().min(3, 'First name most have atleast 3 characters'),
+    address1: z.string().min(3, 'First name most have atleast 3 characters'),
+    address2: z.string().min(3, 'First name most have atleast 3 characters'),
+    phone: z.string().min(3, 'First name most have atleast 3 characters'),
+    country: z.string().min(3, 'First name most have atleast 3 characters'),
+    state: z.string().min(3, 'First name most have atleast 3 characters'),
+
+});
 
 
 export type LoginData = z.infer<typeof loginSchema>;
