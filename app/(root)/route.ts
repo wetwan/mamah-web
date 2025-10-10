504281cb3f5391bc0d3a3f2bdcbafa06aa2fde3e
ridwanullah <wetwan080@gmail.com>
2025-10-10 07:28:06 +0100

    router

--- app/(root)/route.ts
+++ app/(root)/route.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 
 
 
+
+
+
 export const loginUser = async (info: LoginData) => {
     const { data } = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}user/login`, // ✅ remove extra /api/
