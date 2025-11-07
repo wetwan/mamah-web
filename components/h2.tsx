9ba15394ac498bc59cfcd19e5c01af15a52b4f25
ridwanullah <wetwan080@gmail.com>
2025-11-07 23:19:33 +0100

    maybe its the h2

--- components/h2.tsx
+++ components/h2.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import React from "react";
 
 const H2 = ({ text, className }: { text: string; className: any }) => {
   return (
-    <div className={`${className} flex flex-col justify-center w-fit px-3 py-2
-     `}>
+    <div
+      className={`${className} flex flex-col justify-center w-fit px-3 py-2
+     `}
+    >
       <div className="w-12 h-0.5 rounded-full   bg-blue-800" />
-      <h2 className="text-2xl  capitalize font-light mt-2 md:text-4xl md:mt-4 ">{text}</h2>
+      <h2 className="text-2xl  capitalize font-light mt-2 md:text-4xl md:mt-4 ">
+        {text}
+      </h2>
     </div>
   );
 };
